b86f55d6eafd52711918e8bf958f9dfb78ad4957
Daniil Pavlenko <dp3481088@gmail.com>
2020-10-23 01:16:18 +0300

    add README.md

--- src/main.kt
+++ src/main.kt
@@ -1,17 +1,19 @@
 @file: JvmName("main")
 
 fun main(args: Array<String>) {
-    val node: node
-    try {
-        node = readTree(args[0])
-    } catch (e: Exception) {
-        println("Reading file error!")
-        return
-    }
-    try {
-        println(node.fullLine())
-        println(node.Expand())
-    } catch (e: Exception) {
-        println("Running program error!")
+    for (file in args) {
+        val node: node
+        try {
+            node = readTree(file)
+        } catch (e: Exception) {
+            println("Reading file error!")
+            return
+        }
+        try {
+            println(node.fullLine())
+            println(node.Expand())
+        } catch (e: Exception) {
+            println("Running program error!")
+        }
     }
 }
